9329683d8a0b32809f2899af74df2ebebf1d5949
Tanisha Cooper <strickland.tanisha@yahoo.com>
2022-06-23 15:11:41 -0400

    initial commit

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,2 @@
+// Plotly JavaScript Challenge - Belly Button Biodiveristy
+// Step 1. Use D3 library to read in <a href="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.">sample.js</a> 
