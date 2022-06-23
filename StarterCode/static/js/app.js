4d1a3a01d43bfbd5b45d1ed7676480e32f3162d5
Tanisha Cooper <strickland.tanisha@yahoo.com>
2022-06-23 15:42:21 -0400

    app.js d3 read data and bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,2 +1,36 @@
 // Plotly JavaScript Challenge - Belly Button Biodiveristy
-// Step 1. Use D3 library to read in <a href="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.">sample.js</a> 
+// Step 1. Use D3 library to read in sample.json: names, metadata, samples.otu_ids, samples.otu_labels, sample.sample_values
+
+
+function buildCharts(sample) {
+
+d3.json("samples.json").then((data)==> {
+    let samples= data.samples;
+    let resultsarray= samples.filter(sampleobject =>
+        sampleobject.id == sample);
+    let result= resultsarray[0]
+
+    const otu_ids = result.otu_ids;
+    const labels = result.out_lables;
+    const values = result.sample_values;
+
+    // ------------BAR CHART----------//
+
+    let bar_data = [
+        {
+            y:otu_ids.slice(0,10).map(otuID => 'OTU ${otuID}').reverse(),
+            x.values.slice(0,10).reverse(),
+            text:labels.slice(0,10).reverse(),
+            type:"bar",
+            orientation:"h"
+        }
+    ];
+
+    var barLayout = {
+        title: "Top 10 Belly Button Bacteria",
+        margin: {t: 30, l:150}
+    };
+
+    Plotly.newPlot("bar", bar_data, barLayout);
+});
+}
