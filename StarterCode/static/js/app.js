6891ef328f55859fbdc4fee3131c57260bb44bdc
Tanisha Cooper <strickland.tanisha@yahoo.com>
2022-06-24 18:15:55 -0400

    errors fixed

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,88 +1,83 @@
+// const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-function buildMeta(sample){
-    d3.json('./static/js/app.js').then((data)=>{
-        var sample_meta = data.metadata;
-        var results = sample_meta.filter(sampleObject => sampleObject.id==sample);
-        var result = results[0];
-        var panel = d3.select("#sample-metadata");
-        panel.html("");
-        Object.entries(result).forEach(([key, value])=>{
-            panel.append("h6").text('${key.toUpperCase():${value}');
+// d3.json(url).then(function(data){
+//     console.log(data)
+// })
+
+
+function dropdown(){
+    d3.json("./samples.json").then(data => {
+        var sampleName = data.names;
+        var dataid = d3.select("#selDataset")
+        sampleName.forEach((sampleid)=> {
+            dataid.append("option")
+                .text(sampleid)
+                .property("value", sampleid)
+        })
+        var firstSample = sampleName[0];
+        metadata(firstSample);
+        Charts(firstSample);
+    // console.log(sampleId)
+    });
+};
+
+dropdown();
+
+function metadata(id) {
+    d3.json("./samples.json").then(data => {
+        var sampleMetadata = data.metadata;
+        var result = sampleMetadata.filter(obj => obj.id == id);
+        var filterresult = result[0];
+        var display = d3.select("#sample-metadata");
+        display.html("");
+        Object.entries(filterresult).forEach(([key, value]) => {
+            display.append("h6").text(`${key} ${value}`);
         });
     });
 }
 
-buildMeta()
+function Charts(id){
+    d3.json("./samples.json").then(data => {
+        var sampledata = data.samples;
+        var result = sampledata.filter(obj => obj.id == id);
+        var filterresult = result[0];
+        var otu_ids = filterresult.otu_ids;
+        var otu_labels = filterresult.otu_labels;
+        var sample_values = filterresult.sample_values;
+        var bubbledata = [{
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Picnic"
+            }
+        }];
 
-// function buildCharts(sample){
-//     d3.json('../samples.json').then((data)=>{
-//         var samples = data.samples;
-//         var results = samples.filter(sampleObject => sampleObject.id==sample);
-//         var result = results[0];
-//         var otu_ids = result.otu_ids
-//         var otu_labels = result.otu_labels
-//         var sample_values = result.sample_values
+        var barData = [{
+            y: otu_ids.slice(0, 10).map(val => `OTU ${val}`).reverse(),
+            x: sample_values.slice(0, 10).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h",
+        }];
+        Plotly.newPlot("bar", barData);
 
-//         var yticks = otu_ids.slice(0,10).map(x=>'OTU ${x}').reverse();
-//         var barData = [{y:yticks,
-//                         x:sample_values.slice(0,10).reverse(),
-//                         text: otu_labels.slice(0,10).reverse(),
-//                         type: "bar",
-//                         orientation: "h",
-//                         }];
-//         var barLayout = {title: "Top 10 Belly Button Cultures Found",
-//                         margin: {t:30, l:150}};
-//         Plotly.newPlot("bar", barData,barLayout);
+        Plotly.newPlot("bubble", bubbledata);
+    });
+};
 
-// console.log(samples[0].otu_ids)
+function optionChanged(changeid) {
+    metadata(changeid);
+    Charts(changeid);
+};
 
-//         // BUILD BUBBLE CHART
-//         console.log(samples[0].otu_ids)
-//         // build bubble chart
-//         var data_bubble = [{
-//             x: samples[0].otu_ids,
-//             y: samples[0].sample_values,
-//             text:samples[0].otu_labels.slice(0,10),
-//             mode: 'markers',
-//             marker: {
-//               size: samples[0].sample_values,
-//               color: samples[0].otu_ids
-//             }
-//           }];
-//           var layout_bubble = {
-//             title:{
-//               text: ` OTU IDs and Corresponding Sample Values`
-//             } ,
-//             xaxis: {
-//               title: `OTU ID`
-//             },
-//             yaxis: {
-//               title: `Sample Value`
-//             }
-//           };
-      
-//           Plotly.newPlot('bubble', data_bubble,layout_bubble);
-        
-        
-        
-//     });
-// }
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
-// function updatePage(){
-// 	var dropdown=d3.selectAll('#selDataset');
-// 	d3.json('../samples.json').then((data)=>{
-//     var sample_names = data.names
-// 	sample_names.forEach((name)=>{
-// 	dropdown.append('option').text(name).property('value', name);
-// 	});
-//     var firstSample = sample_names[0];
-//     buildCharts(firstSample);
-//     buildMeta(firstSample);
-// 	});
-// }
+// Use sample_values as the values for the bar chart.
 
-// function optionChanged(newSample){
-//     buildCharts(newSample);
-//     buildMeta(newSample);
-// }
-// updatePage();
+// Use otu_ids as the labels for the bar chart.
+
+// Use otu_labels as the hovertext for the chart.
